6fa216b21beb9ce75f716cf1baad01c8d3a4c772
LIMJUNHYOUNG <jhlim@suprema.co.kr>
2025-03-20 16:42:08 +0900

    test10

--- src/main/java/com/example/coupon/config/DataSourceConfig.java
+++ src/main/java/com/example/coupon/config/DataSourceConfig.java
@@ -9,7 +9,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class DataSourceConfig {
-
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariConfig hikariConfig() {
