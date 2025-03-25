bff39b35dc5757444c643e0e591131791bdae127
LIMJUNHYOUNHG <jhlim@suprema.co.kr>
2025-03-25 16:21:19 +0900

    Auto update src/test/java/com/example/coupon/dao/CouponRepository.kt

---
+++ src/test/java/com/example/coupon/dao/CouponRepository.kt
@@ -0,0 +1,7 @@
+class CouponRepositoryTest : StringSpec({
+    "CouponRepository should be testable in the future" {
+        // Placeholder, add actual tests if needed.
+        //  The only change to CouponRepository was adding a newline. So no tests are added for now.
+        1 shouldBe 1
+    }
+})
