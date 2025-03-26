0a201ba18e42cb48e2063fdb20dccce25df62a38
LIMJUNHYOUNHG <jhlim@suprema.co.kr>
2025-03-26 17:05:13 +0900

    Auto update src/test/kotlin/com/example/coupon/controller/CouponControllerTest.kt

---
+++ src/test/kotlin/com/example/coupon/controller/CouponControllerTest.kt
@@ -0,0 +1,12 @@
+import com.example.coupon.controller.CouponController
+import com.example.coupon.controller.CouponCreate
+import com.example.coupon.service.CouponService
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+class CouponControllerTest : StringSpec({
