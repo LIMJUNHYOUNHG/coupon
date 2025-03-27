38bc4fc7aed487598789c51aa70de3e21174c5b8
LIMJUNHYOUNHG <jhlim@suprema.co.kr>
2025-03-27 14:46:41 +0900

    Auto update src/test/kotlin/com/example/coupon/service/BuyServiceTest.kt

---
+++ src/test/kotlin/com/example/coupon/service/BuyServiceTest.kt
@@ -0,0 +1,6 @@
+import com.example.coupon.service.BuyService
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class BuyServiceTest : DescribeSpec({
